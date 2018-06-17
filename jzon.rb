00cc432ae1937f354680d73beb7a88526b0d9621
Ludwig Muench <muench.ludwig@gmail.com>
2018-06-17 19:57:44 +0200

    rename rson_hash to jzon_hash

--- jzon.rb
+++ jzon.rb
@@ -1,6 +1,6 @@
 class Jzon
-  def initialize(rson_hash)
-    rson_hash.each do |key, value|
+  def initialize(jzon_hash)
+    jzon_hash.each do |key, value|
       value = Jzon.new(value) if value.is_a? Hash
       instance_variable_set("@#{key}", value)
       self.class.send(:attr_accessor, key)
